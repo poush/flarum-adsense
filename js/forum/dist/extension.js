3616d9e12ceb03f957d140a73066ef9025ac892e
Piyush Agrawal <poush12@gmail.com>
2016-04-13 09:41:11 +0530

    Intial Commit

---
+++ js/forum/dist/extension.js
@@ -0,0 +1,18 @@
+System.register('adsense/main', ['flarum/extend', 'flarum/app'], function (_export) {
+	'use strict';
+
+	var extend, app;
+	return {
+		setters: [function (_flarumExtend) {
+			extend = _flarumExtend.extend;
+		}, function (_flarumApp) {
+			app = _flarumApp['default'];
+		}],
+		execute: function () {
+
+			app.initializers.add('adsense', function () {
+				alert(" Hello World !");
+			});
+		}
+	};
+});
